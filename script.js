9dbb40ce37265f57f02aac09588d515653480464
Divyamourya04 <116952899+Divyamourya04@users.noreply.github.com>
2024-02-14 18:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //complete this code
-class Rectangle {}
+class Rectangle(width,height) {
+	return=width*height
+	
+}
 
 class Square extends Animal {}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // Output: 5
+console.log(rectangle.height); // Output: 10
+console.log(rectangle.getArea()); // Output: 50
+
